77ffdcf7b8292ee2e5c61c82845cf26f4d73774f
dj git <870419665@qq.com>
2017-04-13 15:11:38 +0800

    good---system.out hello

--- app/src/main/java/com/lide/mygit/MainActivity.java
+++ app/src/main/java/com/lide/mygit/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("");
+        System.out.print("你好啊 我来到了");
     }
 }
