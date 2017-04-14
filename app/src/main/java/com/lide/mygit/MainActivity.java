6f030eb42db7ba56b539422aa85cf17774ea6014
dj git <870419665@qq.com>
2017-04-14 11:23:12 +0800

    Create new branches!

--- app/src/main/java/com/lide/mygit/MainActivity.java
+++ app/src/main/java/com/lide/mygit/MainActivity.java
@@ -2,6 +2,7 @@ package com.lide.mygit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("你好啊 ddddd我来到了");
+        Log.d("test","创建新的分之了!");
     }
 }
