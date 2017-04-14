1e3e6eebf21551328b97f68b38ef1138861254e0
dj git <870419665@qq.com>
2017-04-14 13:37:17 +0800

    add branch_1!

--- app/src/main/java/com/lide/mygit/MainActivity.java
+++ app/src/main/java/com/lide/mygit/MainActivity.java
@@ -2,7 +2,6 @@ package com.lide.mygit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("你好啊 ddddd我来到了");
-        Log.d("test","创建新的分之了!");
     }
 
-    public void  createFeature_1(){
-
-    }
+    public void  addBranch_1(){};
 }
